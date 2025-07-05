701abf24131570f16b08e45afe7952ec29399098
Dionisio Tejeda <tejedawx@dionisios-mini.home>
2025-07-05 09:53:38 -0400

    fixing the folders adding for the server

--- TWO_Script/main.py
+++ TWO_Script/main.py
@@ -4,10 +4,13 @@ import pathlib
 import argparse
 from datetime import datetime, timezone
 from two_plot import build_outlook
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from facebook_poster import post_to_facebook
 from gtwo_fetcher import download_gtwo_shapefile
 import matplotlib.pyplot as plt
+import platform
+import shutil
+import os
 
 with open("cron_test.log", "a") as f:
     f.write("Triggered at: " + datetime.now(timezone.utc).isoformat() + "\n")
@@ -41,6 +44,29 @@ def main():
             download_gtwo_shapefile(basin_tag)
             out_path = build_outlook(basin_tag, label, prefix, outdir, ts)
             print(f"✅ Saved: {out_path.relative_to(outdir.parent)}")
+            
+         
+            # only copy on Linux
+            if platform.system() == "Linux":
+                # everything that copies to /var/www/html/output
+                today_str = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+                target_dir = f"/var/www/html/output/{today_str}"
+                os.makedirs(target_dir, exist_ok=True)
+                shutil.copy(str(out_path), target_dir)
+
+                # cleanup
+                output_base = "/var/www/html/output"
+                for folder in os.listdir(output_base):
+                    folder_path = os.path.join(output_base, folder)
+                    if os.path.isdir(folder_path):
+                        try:
+                            folder_date = datetime.strptime(folder, "%Y-%m-%d")
+                            if folder_date < datetime.now(timezone.utc) - timedelta(days=6):
+                                shutil.rmtree(folder_path)
+                                print(f"Removed old folder: {folder_path}")
+                        except ValueError:
+                            continue
+
 
             # 🔽 NEW: Post to Facebook
             #post_to_facebook(
